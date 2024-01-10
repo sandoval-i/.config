d9e95e49f565b6ba9660c8e1f2ffd6a62546ea04
sandoval-i <sandoval.i@javeriana.edu.co>
2024-01-09 20:07:09 -0500

    Little adjustments for laptop config to work on desktop

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -23,7 +23,7 @@ end)
 -- setup language servers
 require("mason").setup({})
 require("mason-lspconfig").setup({
-  ensure_installed = { "clangd", "lua_ls", "tsserver" },
+  ensure_installed = { "clangd", "lua_ls" },
   handlers = {
     lsp_zero.default_setup,
     clangd = function()
