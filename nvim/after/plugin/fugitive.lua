c42fb4fe683fd04cfc8b74edefac656f3ce41c95
Ivan Dario Sandoval Acosta <sandoval.i@javeriana.edu.co>
2024-01-09 19:25:52 -0500

    Remove unneccesary git-fugitive keymap

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,4 +1,3 @@
 local keymap = vim.keymap;
 keymap.set("n", "<leader>git", vim.cmd.Git, { desc = "Git-fugitive plugin" })
 keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "[g]it [b]lame" })
-keymap.set("n", "<leader>gd", ":Git diff<CR>", { desc = "[g]it [d]iff" })
