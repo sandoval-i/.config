8b7bb6c083ba8815a20adf4bc42069b0b1f1806d
sandoval-i <sandoval.i@javeriana.edu.co>
2025-08-09 23:36:23 -0500

    Fix deprecated lsp config + diagnostic functions

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,45 +15,18 @@ return {
       nvim_lspconfig,
       blink,
     },
-    opts = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-      return {
-        ensure_installed = {
-          "bashls",
-          "clangd",
-          "dockerls",
-          "jsonls",
-          "lua_ls",
-          "pyright",
-          "terraformls",
-          "ts_ls",
-        },
-        handlers = {
-          clangd = function()
-            lspconfig.clangd.setup({
-              capabilities = capabilities,
-              init_options = {
-                fallbackFlags = { "-std=c++20" },
-              },
-            })
-          end,
-
-          lua_ls = function()
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
-          end,
-
-          pyright = function()
-            lspconfig.pyright.setup({ capabilities = capabilities })
-          end,
-
-          ts_ls = function()
-            lspconfig.ts_ls.setup({ capabilities = capabilities })
-          end,
-        },
-      }
-    end,
+    opts = {
+      ensure_installed = {
+        "bashls",
+        "clangd",
+        "dockerls",
+        "jsonls",
+        "lua_ls",
+        "pyright",
+        "terraformls",
+        "ts_ls",
+      },
+    },
     config = function(_, opts)
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
@@ -64,10 +37,10 @@ return {
             vim.lsp.buf.rename()
           end, { buffer = args.buf, desc = "[l]sp [r]e[n]ame" })
           vim.keymap.set("n", "]d", function()
-            vim.diagnostic.goto_next()
+            vim.diagnostic.jump({ count = 1, float = true })
           end, { buffer = args.buf, desc = "]next [d]iagnostic" })
           vim.keymap.set("n", "[d", function()
-            vim.diagnostic.goto_prev()
+            vim.diagnostic.jump({ count = -1, float = true })
           end, { buffer = args.buf, desc = "[prev [d]iagnostic" })
         end,
       })
